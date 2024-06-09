6d92d3328ca55028065ed348a761ca3948232d9d
J-Bills <billsjamal@gmail.com>
2024-06-08 20:53:05 -0700

    creating basic login page with wtForms

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import fields
+from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect
+
 
 '''
 Red underlines? Install the required packages first: 
@@ -13,13 +18,28 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+class MyForm(FlaskForm):
+    password = fields.PasswordField(label='Password', validators=[DataRequired()])
+    email = fields.StringField(label='Email', validators=[DataRequired()])
+    submit = fields.SubmitField(label='Log in')
+    
+
 
 app = Flask(__name__)
+app.secret_key = "randomjunk"
+csrf = CSRFProtect(app)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
